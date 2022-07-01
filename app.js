eb9e0a6ffeb6dbec98c04b652b13fad402ec133d
aiexgt <lima14florian14@gmail.com>
2022-07-01 10:02:02 -0600

    Conexion bd

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+require("dotenv").config();
 
 app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
 
 app.get("/", (req, res) => {
     res.send("Hola mundo");
